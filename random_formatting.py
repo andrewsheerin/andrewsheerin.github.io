d9d114634d08d64016c6fd7d9631c1d4f9140beb
andrewsheerin <andrew_sheerin@uri.edu>
2024-07-23 16:33:22 -0400

    some updates

--- random_formatting.py
+++ random_formatting.py
@@ -1,16 +1,5 @@
 # reformat this image to be 300px by 300px
-
 from PIL import Image
+# slow down the gif to last 5 seconds
 
-image = r"C:\Users\flow\PycharmProjects\andrewsheerin.github.io\images\storymap.png"
-img = Image.open(image)
-img = img.resize((300, 300))
-
-# overlay image/img.png on top of image
-img2 = r"C:\Users\flow\PycharmProjects\andrewsheerin.github.io\images\img.png"
-img2 = Image.open(img2)
-img.paste(img2, (0, 0))
-
-
-img.save(r"C:\Users\flow\PycharmProjects\andrewsheerin.github.io\images\storymap_300.png")
 
