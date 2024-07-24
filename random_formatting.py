fd32f9ed98a35cdee0ad15cf0c6e7c9f8557bea6
andrewsheerin <andrew_sheerin@uri.edu>
2024-07-24 15:52:56 -0400

    some updates

--- random_formatting.py
+++ random_formatting.py
@@ -1,5 +1,9 @@
-# reformat this image to be 300px by 300px
-from PIL import Image
-# slow down the gif to last 5 seconds
+from PIL import Image, ImageSequence
 
+# convert png to jpg
+def png_to_jpg(png_path, jpg_path):
+    im = Image.open(png_path)
+    im = im.convert('RGB')
+    im.save(jpg_path)
 
+png_to_jpg('images/headshot_circle.png', 'images/headshot_circle.jpg')
